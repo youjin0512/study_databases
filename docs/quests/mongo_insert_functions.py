5fa865893b98860672d6ae2edebf13d53f458a0b
youjin0512 <kelly2.youjin.kim@gmail.com>
2023-12-14 10:21:41 +0900

    modify

--- docs/quests/mongo_insert_functions.py
+++ docs/quests/mongo_insert_functions.py
@@ -14,7 +14,7 @@ def insert(collection,dict_fruit):
     return
 
 fruit_info = [
-            {"name": "수박", "color": "초록", "origin": "한국", "price_per_kg": 15000},
+            {"name": "사과", "color": "빨강", "origin": "한국", "price_per_kg": 4000},
             {"name": "바나나", "color": "노랑", "origin": "필리핀", "price_per_kg": 3000},
             {"name": "오렌지", "color": "주황", "origin": "미국", "price_per_kg": 5000},
             {"name": "포도", "color": "보라", "origin": "한국", "price_per_kg": 8000},
