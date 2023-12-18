3f00b20da36969fa9cad57c84ed9315311ee51b7
youjin0512 <kelly2.youjin.kim@gmail.com>
2023-12-18 15:02:04 +0900

    main

--- docs/quests/todolist_main.py
+++ docs/quests/todolist_main.py
@@ -1,8 +1,8 @@
-import todos_list_functions # todos_list_functions라는 파이썬 파일(모듈)을 호출한다.
+import todolist_functions # todos_list_functions라는 파이썬 파일(모듈)을 호출한다.
 
-collection = todos_list_functions.connect() #호출한 파읠의 Connect라는 function을 호출하여 collection이라는 변수로 지정한다.
+todolist_functions.connect() #호출한 파읠의 Connect라는 function을 호출하여 collection이라는 변수로 지정한다.
 
-todos_list_functions.todo_list()
+todolist_functions.todo_list()
 
-todos_list_functions.run() 
+todolist_functions.run() 
 # 위 과정을 실행하는 function을 호출하여 동작시킨다.
