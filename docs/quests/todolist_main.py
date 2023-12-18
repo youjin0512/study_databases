35ac7857fb12c538290bade75409ef5582b77716
youjin0512 <kelly2.youjin.kim@gmail.com>
2023-12-18 15:03:20 +0900

    modify_main

--- docs/quests/todolist_main.py
+++ docs/quests/todolist_main.py
@@ -1,6 +1,6 @@
 import todolist_functions # todos_list_functions라는 파이썬 파일(모듈)을 호출한다.
 
-todolist_functions.connect() #호출한 파읠의 Connect라는 function을 호출하여 collection이라는 변수로 지정한다.
+todolist_functions.connect() #호출한 파일의 Connect라는 function을 호출
 
 todolist_functions.todo_list()
 
