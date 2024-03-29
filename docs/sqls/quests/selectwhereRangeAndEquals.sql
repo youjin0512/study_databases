629014f9a505bf072611b62fcd401b0d1d5f5a4c
youjin0512 <kelly2.youjin.kim@gmail.com>
2024-03-29 15:47:16 +0900

    modify

--- docs/sqls/quests/selectwhereRangeAndEquals.sql
+++ docs/sqls/quests/selectwhereRangeAndEquals.sql
@@ -3,9 +3,16 @@
 -- 조건 : Country 가 Germany 와 USA가 아닌 것들
 SELECT *
 FROM Customers
-WHERE Country NOT IN ('Germany', 'USA');    -- COUNT : 67
+WHERE Country NOT IN ('Germany', 'USA');    -- Number of Records: 67
 
 -- 조건 : CustomerID가 50에서 89이고, City가 London인 고객
+-- 방법1)
 SELECT *
 FROM Customers
-WHERE (CustomerID BETWEEN 50 AND 89) AND (City = 'London');   -- COUNT : 2
+WHERE (CustomerID BETWEEN 50 AND 89) AND (City = 'London');   -- Number of Records: 2
+
+-- 방법2)
+SELECT CustomerID, City
+FROM Customers
+WHERE CustomerID BETWEEN 50 AND 89
+AND City = 'London';    -- Number of Records: 2
