3d855e3254a80a87e9a96837446d827249a51da4
youjin0512 <kelly2.youjin.kim@gmail.com>
2024-03-29 15:49:27 +0900

    modify

--- docs/sqls/quests/selectwhereRangeAndEquals.sql
+++ docs/sqls/quests/selectwhereRangeAndEquals.sql
@@ -1,8 +1,8 @@
--- refer : https://www.w3schools.com/sql/sql_where.asp
+-- refer : https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_columns
 
 -- 조건 : Country 가 Germany 와 USA가 아닌 것들
 SELECT *
-FROM Customers
+FROM Customers                              -- Number of Records: 91
 WHERE Country NOT IN ('Germany', 'USA');    -- Number of Records: 67
 
 -- 조건 : CustomerID가 50에서 89이고, City가 London인 고객
